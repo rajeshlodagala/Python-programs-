b564b5931c395cdecd838c4c3ee72ad1130f731e
rajeshlodagala <rajeshlodagala047@gmail.com>
2025-08-28 18:01:37 +0530

    Added sum program

---
+++ sum_natural.py
@@ -0,0 +1,9 @@
+# Program to find sum of first N natural numbers
+
+# Taking input from user
+n = int(input("Enter a number: "))
+
+# Formula method
+sum_n = n * (n + 1) // 2
+
+print("Sum of first", n, "natural numbers is:", sum_n)
